6e4971f9b4fabaccb5fbaba57183ca1eec8eb293
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-04-19 17:07:11 +1000

    same in the mapper

--- bookstore/src/main/java/com/thinkconstructive/bookstore/mapper/BookMapper.java
+++ bookstore/src/main/java/com/thinkconstructive/bookstore/mapper/BookMapper.java
@@ -7,7 +7,7 @@ public class BookMapper {
 
     public static BookDto toDto(Book book) 
     {
-        BookDto bookDto = new BookDto(book.bookId(), book.name(), book.price(),
+        BookDto bookDto = new BookDto(book.bookID(), book.name(), book.price(),
                             book.author(), book.description());
 
         return bookDto;
@@ -15,7 +15,7 @@ public class BookMapper {
 
     public static Book toEntity(BookDto bookDto) 
     {
-        Book book = new Book(bookDto.bookId(), bookDto.name(), bookDto.price(),
+        Book book = new Book(bookDto.bookID(), bookDto.name(), bookDto.price(),
                             bookDto.author(), bookDto.description());
 
         return book;                    

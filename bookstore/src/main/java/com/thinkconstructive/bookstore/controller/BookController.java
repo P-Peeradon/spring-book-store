3f866352d3c858ad590340b7a30ea036bbd43e20
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-04-26 15:36:26 +1000

    feat: welcome message in controller

--- bookstore/src/main/java/com/thinkconstructive/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/thinkconstructive/bookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,13 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcomeMessage() 
+    {
+        return new ResponseEntity<>("Welcome to the Book Store", HttpStatus.OK);
+    }
+    
 
     @GetMapping("/{bookID}")
     public ResponseEntity<BookDto> getBook(@PathVariable String bookID) {

59e0147449a5308d7944d71a14ab258d955af481
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-04-26 13:57:27 +1000

    feat: implement  service by defining data body.

--- bookstore/src/main/java/com/thinkconstructive/bookstore/service/impl/BookServiceImpl.java
+++ bookstore/src/main/java/com/thinkconstructive/bookstore/service/impl/BookServiceImpl.java
@@ -1,8 +1,12 @@
 package com.thinkconstructive.bookstore.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.thinkconstructive.bookstore.dto.BookDto;
+import com.thinkconstructive.bookstore.entity.Book;
+import com.thinkconstructive.bookstore.mapper.BookMapper;
+import com.thinkconstructive.bookstore.repository.BookRepository;
 import com.thinkconstructive.bookstore.service.BookService;
 
 import org.springframework.stereotype.Service;
@@ -11,29 +15,53 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl implements BookService 
 {
 
-    @Override
-    public BookDto getBook(String bookID) {
-        return null;
+    BookRepository bookRepository;
+
+
+    public BookServiceImpl(BookRepository bookRepository) 
+    {
+        this.bookRepository = bookRepository;
     }
 
     @Override
-    public List<BookDto> getAllBooks() {
-        return null;
+    public BookDto getBook(String bookID) 
+    {
+        Book book = bookRepository.findByID(bookID);
+        BookDto bookDto = BookMapper.toDto(book);
+        return bookDto;
     }
 
     @Override
-    public BookDto createBook(BookDto bookDto) {
-        return null;
+    public List<BookDto> getAllBooks() 
+    {
+        List<Book> books = bookRepository.findAll();
+        List<BookDto> bookDtoList = new ArrayList<>();
+        
+        for (Book book: books) 
+        {
+            bookDtoList.add(BookMapper.toDto(book));
+        }
+
+        return bookDtoList;
+    }
+
+    @Override
+    public BookDto createBook(BookDto bookDto) 
+    {
+        Book book = bookRepository.insert(BookMapper.toEntity(bookDto));
+        return BookMapper.toDto(book);
     }
 
     @Override
     public BookDto updateBook(BookDto bookDto) {
-        return null;
+        bookRepository.updateNameByID(bookDto.bookID(), bookDto.name());
+        Book book = bookRepository.findByID(bookDto.bookID());
+        return BookMapper.toDto(book);
     }
 
     @Override
     public void deleteBookByID(String bookID) {
-        
+        bookRepository.deleteByBookID(bookID);
     }
 
 }
